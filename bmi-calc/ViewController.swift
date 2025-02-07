9885441a8afe394f70f28d0898eb53b87829bfd6
James Chard N01685334 <n01685334@humber.ca>
2025-02-07 13:05:31 -0500

    Initial Commit

---
+++ bmi-calc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bmi-calc
+//
+//  Created by James Chard on 2025/2/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
